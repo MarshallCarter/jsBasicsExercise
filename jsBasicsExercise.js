7e8478d24b8c931587d9c1fd2b5b49b21258a515
Marshall Carter <marshallcarter@SCWDBs-MacBook-Pro-2.local>
2021-02-22 13:32:52 -0600

    JavaScript Basics Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,25 @@
+console.log(7);
+
+/*
+Numbers
+Strings
+Booleans
+Null
+Undefined
+*/
+
+// +, -, *, /, **, %
+
+console.log(5%4);
+console.log(5%2);
+
+console.log("'I'm Mary Poppins, Y'all'");
+
+console.log("Wanda" + " " + "Vision");
+
+console.log("Marshall" .length );
+console.log("Carter".length);
+console.log("Marshall" .length + "Carter".length);
+
+console.log("Avengers: Endgame"[16]);
+
